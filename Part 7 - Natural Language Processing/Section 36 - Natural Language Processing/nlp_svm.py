a342e476ac774fe288b0e2dba1f820e19affba8a
shreyas <hello@shreyas.im>
2018-02-15 00:45:48 +0530

    finished nlp homework

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/nlp_svm.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/nlp_svm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb  3 08:35:14 2018
+Created on Wed Feb 14 23:47:48 2018
 
 @author: shreyas
 """
@@ -11,29 +11,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-# quoting = 3 ignores any double quotes
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
 # CLeaning the texts
 import re
 import nltk
-# nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-# corpus is a collection of text in ml terms
 corpus = []
-# range(0, 1000) - 0 is the lower bound, 1000 is the upper bound
 for i in range(0, 1000):
-    # dataset['Review'][0] gives the first review
-    # remove anything that is not a-z or A-Z. ' ' (second parameter) is so that spaces are not removed making the words stick together
     review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
-    # Making all the words lowercase
     review = review.lower()
     review = review.split()
-    # PorterStemmer is to stem the words. That is, to change the words to their original form. Eg: loved becomes love
     ps = PorterStemmer()
-    # iterating through review and checking if any word from stopwords exists. if it exists, then remove it. finally stem all the words
-    # Note: if the sentence is big, use set as algorithms in python work faster when going through words in set than list
     review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)
     corpus.append(review)
@@ -41,8 +31,6 @@ for i in range(0, 1000):
 # Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
-# X is a sparse matrix (matrix with mostly 0s is a sparse matrix). It is the independent variable
-# X has to be fit to the corpus, to analyze it and to look at all the words, look at how to apply different parameters we input in count vectorizer
 X = cv.fit_transform(corpus).toarray()
 
 # y is the dependent variable
@@ -52,9 +40,9 @@ y = dataset.iloc[:, 1].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -64,7 +52,7 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Homework - Calculating Accuracy, Precision, Recall and F1 Score
+# Calculating Accuracy, Precision, Recall and F1 Score
 true_positive = cm[1][1]
 false_positive = cm[1][0]
 
